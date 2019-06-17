947bcfedd7dc753a6b095c995d09803315b8ddf3
Jamie Zhang <akjamie.zhang@outlook.com>
2019-06-17 23:28:11 +0800

    update project config

--- src/main/java/org/akj/springboot/inventory/Application.java
+++ src/main/java/org/akj/springboot/inventory/Application.java
@@ -2,13 +2,17 @@ package org.akj.springboot.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Springboot application - Entry point
  * @author Jamie Y L Zhang
  */
 
-@SpringBootApplication
+@EnableCircuitBreaker
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "org.akj.springboot")
 public class Application {
 	
 	public static void main(String[] args) {
